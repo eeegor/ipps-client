6dfc19ca285ab316a2b2c8d4eff35efb52adce15
Egor Kirpichev <egor.kirpichev@gmail.com>
2018-12-15 19:47:45 +0100

    refactor: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,26 @@ import { Title, Text } from './components';
 import './App.scss';
 import axios from 'axios';
 
-function getParameterByName(name, url) {
-  if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, '\\$&');
-  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-    results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
-
 export class App extends Component {
   state = {
     requests: {},
     providers: [],
     errors: []
   };
+
+  setError = error =>
+    this.setState(state => ({ ...state, errors: [...state.errors, error] }));
+
+  setProviders = providers => this.setState(state => ({ ...state, providers }));
+  
+  setRequestStatus = (status, callback) =>
+    this.setState(
+      state => ({
+        ...state,
+        requests: { ...state.requests, getProviders: status }
+      }),
+      callback
+    );
 
   async getProviders() {
     const urlParams = new URLSearchParams(window.location.search);
@@ -35,29 +39,10 @@ export class App extends Component {
     await axios
       .get(`https://ipps-api.now.sh/providers?${query}`)
       .then(res => {
-        this.setState(
-          state => ({
-            ...state,
-            requests: { ...state.requests, getProviders: 'success' }
-          }),
-          () => {
-            this.setState(state => ({ ...state, providers: res.data }));
-          }
-        );
+        this.setRequestStatus('success', () => this.setProviders(res.data));
       })
       .catch(error =>
-        this.setState(
-          state => ({
-            ...state,
-            requests: { ...state.requests, getProviders: 'failed' }
-          }),
-          () => {
-            this.setState(state => ({
-              ...state,
-              errors: [...state.errors, error]
-            }));
-          }
-        )
+        this.setRequestStatus('failed', () => this.setError(error))
       );
   }
 
