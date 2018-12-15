2c0ae5d64a0ccd000838c7734137dde20750737a
Egor Kirpichev <egor.kirpichev@gmail.com>
2018-12-15 17:34:51 +0100

    fix: replace empty hash stylemock with `identity-obj-proxy`

--- jest.config.js
+++ jest.config.js
@@ -30,6 +30,6 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/test/__mocks__/fileMock.js',
-    '\\.(css|scss)$': '<rootDir>/test/__mocks__/styleMock.js'
+    '\\.(css|scss)$': 'identity-obj-proxy'
   }
 };
