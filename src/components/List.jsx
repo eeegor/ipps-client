2373759e67b4c7d8c6d6456656eb9e1bf68a7a0d
Egor Kirpichev <egor.kirpichev@gmail.com>
2018-12-16 19:23:14 +0100

    component: list

---
+++ src/components/List.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { AutoSizer, Table, Column, SortDirection } from 'react-virtualized';
+import { sortBy as lodashSortBy } from 'lodash';
+import './List.css';
+
+export class List extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sortBy: this.props.columns[0].dataKey || this.props.columns[0].label,
+      sortDirection: SortDirection.DESC,
+      sortedItems: lodashSortBy(
+        this.props.items,
+        this.props.columns[0].dataKey || this.props.columns[0].label
+      )
+    };
+
+    this._sort = this._sort.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    const prev = prevProps.items || null;
+    const next = this.props.items || null;
+    if (prev !== next) {
+      this.setState({
+        sortedItems: lodashSortBy(this.props.items, this.state.sortBy)
+      });
+    }
+  }
+
+  _sort(sortProps) {
+    const { sortBy, sortDirection } = sortProps;
+    const sortedItems = this._getSortedList(
+      this.state.sortedItems,
+      sortBy,
+      sortDirection
+    );
+
+    if (sortedItems.length > 0) {
+      this.setState({
+        sortBy,
+        sortDirection,
+        sortedItems
+      });
+    }
+  }
+
+  _getSortedList(items, sortBy, sortDirection) {
+    const key = sortBy;
+    const currencyKeys = [
+      'Average Covered Charges',
+      'Average Total Payments',
+      'Average Medicare Payments'
+    ];
+    const sorted = lodashSortBy(items, item =>
+      currencyKeys.includes(key)
+        ? parseFloat(item[key].replace(/[$|,]/g, ''))
+        : item[key]
+    );
+    return sortDirection === SortDirection.ASC ? sorted : sorted.reverse();
+  }
+
+  render() {
+    const { sortBy, sortDirection, sortedItems } = this.state;
+    const { columns } = this.props;
+    const cols = columns.map((columnProps, index) => (
+      <Column
+        key={index}
+        width={1}
+        dataKey={columnProps['dataKey'] || columnProps.label}
+        {...columnProps}
+      />
+    ));
+    const addedWidth = columns.reduce((acc, current) => acc + current.width, 0);
+
+    return (
+      <AutoSizer className="ReactVirtualized__AutoSizer">
+        {({ height, width }) => (
+          <Table
+            headerHeight={40}
+            rowCount={sortedItems.length}
+            rowGetter={({ index }) => sortedItems[index]}
+            rowHeight={40}
+            width={2200}
+            height={height}
+            sort={this._sort}
+            sortBy={sortBy}
+            sortDirection={sortDirection}
+          >
+            {cols}
+          </Table>
+        )}
+      </AutoSizer>
+    );
+  }
+}
